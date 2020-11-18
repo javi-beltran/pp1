6e0d4f8f93cf430b28fdd40f6bd4a81b8ea99488
javi-beltran <72995772+javi-beltran@users.noreply.github.com>
2020-11-17 23:34:38 -0300

    Add files via upload

--- carusel1.js
+++ carusel1.js
@@ -6,37 +6,34 @@ if (foto==1){
     document.getElementById("b1").hidden=true;
     document.getElementById("t1").hidden=true;
     document.getElementById("ba1").hidden=true;
-
     document.getElementById("b2").hidden=false;
     document.getElementById("t2").hidden=false;
     document.getElementById("ba2").hidden=false;
-    foto=2;
+    foto==2;
 }else if(foto==2){
     document.getElementById("b2").hidden=true;
     document.getElementById("t2").hidden=true;
     document.getElementById("ba2").hidden=true;
-
     document.getElementById("b3").hidden=false;
     document.getElementById("t3").hidden=false;
     document.getElementById("ba3").hidden=false;
-    foto=3;
+    foto==3;
 }else if(foto==3){
     document.getElementById("b3").hidden=true;
     document.getElementById("t3").hidden=true;
     document.getElementById("ba3").hidden=true;
-
     document.getElementById("b4").hidden=false;
     document.getElementById("t4").hidden=false;
     document.getElementById("ba4").hidden=false;
-    foto=4;
-}else if(foto=4){
+    foto==4;
+}else if(foto==4){
     document.getElementById("b4").hidden=true;
     document.getElementById("t4").hidden=true;
     document.getElementById("ba4").hidden=true;
     document.getElementById("b1").hidden=false;
     document.getElementById("t1").hidden=false;
     document.getElementById("ba1").hidden=false;
-    foto=1;
+    foto==1;
 }
 }
 
